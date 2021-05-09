3e1c11a6aa61bf5a0a28a0b9fcddcaba162aa2ae
User <2176589@mail.ru>
2021-05-08 22:05:20 -0700

    Global refactor

--- js.js
+++ js.js
@@ -1,82 +1,89 @@
-let occupiedCells = [];
-let cellsWithCross = [];
-let cellsWithZero = [];
+const CROSS_CLASS = 'cross'
+const ZERO_CLASS = 'zero'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('.cell')
+const block = document.getElementById('block')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('.end-game-message')
+let circleTurn
 
+startGame()
 
-userTurn();
+restartButton.addEventListener('click', startGame)
 
-
-function userTurn() {
-    document.getElementById('block').onclick = function (event) {
-        let cell_text = event.target.innerHTML;
-        let occupiedCross = document.getElementsByClassName('cell');
-        if (cell_text !== "") {
-            alert("occupied!");
-        } else {
-            event.target.innerHTML = 'X';
-            for (let i = 0; i < 9; i++) {
-                {
-                    if (occupiedCross[i].innerHTML === 'X' && occupiedCells.includes(i) === false) {
-                        occupiedCells.push(i);
-                        cellsWithCross.push(i);
-                        isGameOver(occupiedCells);
-                    }
-                }
-            }
-            computerTurn();
-            checkWinner();
-        }
-    }
-
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(CROSS_CLASS)
+        cell.classList.remove(ZERO_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, {once: true})
+    })
+    setblockHoverClass()
+    winningMessageElement.classList.remove('show')
 }
 
-
-function computerTurn() {
-    let number = Math.floor(Math.random() * 9);
-
-    while (occupiedCells.includes(number) === true) {
-        number = Math.floor(Math.random() * 9);
-    }
-    let classes = [];
-
-    Array.from(document.querySelector('#block').children).forEach(e => classes.push(e.classList.toString()));
-    document.querySelector('#block').children[number].innerHTML = "O";
-    let occupiedZero = document.getElementsByClassName('cell');
-    for (let i = 0; i < 9; i++) {
-        if (occupiedZero[i].innerHTML === 'O' && occupiedCells.includes(i) === false) {
-            occupiedCells.push(i);
-            cellsWithZero.push(i);
-            isGameOver(occupiedCells);
-            break
-        }
-
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? ZERO_CLASS : CROSS_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setblockHoverClass()
     }
 }
 
-function checkWinner() {
-    let winSells = document.getElementsByClassName('cell');
-    if (winSells[0].innerHTML === 'X' && winSells[1].innerHTML === 'X' && winSells[2].innerHTML === 'X') alert('crosses won')
-    if (winSells[3].innerHTML === 'X' && winSells[4].innerHTML === 'X' && winSells[5].innerHTML === 'X') alert('crosses won')
-    if (winSells[6].innerHTML === 'X' && winSells[7].innerHTML === 'X' && winSells[8].innerHTML === 'X') alert('crosses won')
-    if (winSells[0].innerHTML === 'X' && winSells[3].innerHTML === 'X' && winSells[6].innerHTML === 'X') alert('crosses won')
-    if (winSells[1].innerHTML === 'X' && winSells[4].innerHTML === 'X' && winSells[7].innerHTML === 'X') alert('crosses won')
-    if (winSells[2].innerHTML === 'X' && winSells[5].innerHTML === 'X' && winSells[8].innerHTML === 'X') alert('crosses won')
-    if (winSells[0].innerHTML === 'X' && winSells[4].innerHTML === 'X' && winSells[8].innerHTML === 'X') alert('crosses won')
-    if (winSells[2].innerHTML === 'X' && winSells[4].innerHTML === 'X' && winSells[6].innerHTML === 'X') alert('crosses won')
-
-    if (winSells[0].innerHTML === 'O' && winSells[1].innerHTML === 'O' && winSells[2].innerHTML === 'O') alert('zero won')
-    if (winSells[3].innerHTML === 'O' && winSells[4].innerHTML === 'O' && winSells[5].innerHTML === 'O') alert('zero won')
-    if (winSells[6].innerHTML === 'O' && winSells[7].innerHTML === 'O' && winSells[8].innerHTML === 'O') alert('zero won')
-    if (winSells[0].innerHTML === 'O' && winSells[3].innerHTML === 'O' && winSells[6].innerHTML === 'O') alert('zero won')
-    if (winSells[1].innerHTML === 'O' && winSells[4].innerHTML === 'O' && winSells[7].innerHTML === 'O') alert('zero won')
-    if (winSells[2].innerHTML === 'O' && winSells[5].innerHTML === 'O' && winSells[8].innerHTML === 'O') alert('zero won')
-    if (winSells[0].innerHTML === 'O' && winSells[4].innerHTML === 'O' && winSells[8].innerHTML === 'O') alert('zero won')
-    if (winSells[2].innerHTML === 'O' && winSells[4].innerHTML === 'O' && winSells[6].innerHTML === 'O') alert('zero won')
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
 }
 
-const isGameOver = (arr) => {
-    if (arr.length === 9) {
-        arr.length = 0
-        alert("game is over");
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(CROSS_CLASS) || cell.classList.contains(ZERO_CLASS)
+    })
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setblockHoverClass() {
+    block.classList.remove(CROSS_CLASS)
+    block.classList.remove(ZERO_CLASS)
+    if (circleTurn) {
+        block.classList.add(ZERO_CLASS)
+    } else {
+        block.classList.add(CROSS_CLASS)
     }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
